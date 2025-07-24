1b04726a55bf1ba9655b34998585826daf69c438
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-24 00:46:38 -0400

    Completed C# story program exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CreateStory();
+        }
+
+        static string Question(string text)
+        {
+            // Write() instead of WriteLine() to allow
+            // same line reply. ReadLine provides a new line
+            // after input.
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+
+        static void CreateStory()
+        {
+            string userName = Question("What is your name: ");
+            string color = Question("What is your favorite color: ");
+            string animal = Question("What is your favorite animal: ");
+            string band = Question("What is your favorite band: ");
+
+            Console.WriteLine($"Your name is {userName}.\n"
+                + $"Your favorite color is {color}.\n"
+                + $"Your favorite animal is {animal}.\n"
+                + $"Your favorite band is {band}.");
         }
     }
 }
