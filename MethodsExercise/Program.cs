28c6fa7c2ee31548a52ca72159b2bda077b73d36
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-24 01:01:54 -0400

    Completed Exercise 2 with math operations methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             CreateStory();
+
+            DoMath();
         }
 
         static string Question(string text)
@@ -27,6 +29,54 @@
                 + $"Your favorite color is {color}.\n"
                 + $"Your favorite animal is {animal}.\n"
                 + $"Your favorite band is {band}.");
+        }
+
+        static int Add(params int[] values)
+        {
+            int total = values[0];
+
+            foreach (int value in values.Skip(1))
+                total += value;
+
+            return total;
+        }
+
+        static int Subtract(params int[] values)
+        {
+            int total = values[0];
+
+            foreach (int value in values.Skip(1))
+                total -= value;
+
+            return total;
+        }
+
+        static int Multiply(params int[] values)
+        {
+            int total = values[0];
+
+            foreach (int value in values.Skip(1))
+                total *= value;
+
+            return total;
+        }
+
+        static int Divide(params int[] values)
+        {
+            int total = values[0];
+
+            foreach (int value in values.Skip(1))
+                total /= value;
+
+            return total;
+        }
+
+        static void DoMath()
+        {
+            Console.WriteLine("2 + 4 + 6 = " + Add(2, 4, 6).ToString());
+            Console.WriteLine("2 - 4 - 1 = " + Subtract(2, 4, 1).ToString());
+            Console.WriteLine("2 * 4 * 3 = " + Multiply(2, 4, 3).ToString());
+            Console.WriteLine("8 / 2 / 2 = " + Divide(8, 2, 2).ToString());
         }
     }
 }
